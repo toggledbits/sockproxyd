95b316618635109687f59434732c358e08493f95
Patrick Rigney <patrick@toggledbits.com>
2020-04-10 13:53:58 -0400

    New

---
+++ init-script.sh
@@ -0,0 +1,15 @@
+#!/bin/sh /etc/rc.common
+
+START=80
+
+USE_PROCD=1
+PROG=/etc/cmh-ludl/sockproxyd.lua
+LOGFILE=/tmp/log.sockproxyd
+
+start_service () {
+        procd_open_instance
+        procd_set_param command lua "$PROG" -L "$LOGFILE"
+        procd_set_param pidfile /var/run/sockproxyd.pid
+        procd_set_param limits core="unlimited"
+        procd_close_instance
+}
